c79a7a9fc4805d842d218e52087663c7c1ce815c
KatyaKlema <katyaklema@gmail.com>
2020-08-12 22:35:23 +0300

    changed favicon and title and packages.json

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Profile from "./components/profile/profile";
 import "./app.scss";
-
+import { Helmet } from 'react-helmet'
 function App() {
   return (
     <div className="App">
+        <link rel="shortcut icon" href="star.png" />
+        <Helmet>
+          <title>{"Kate Klem"}</title>
+        </Helmet>
         <Profile/>
     </div>
   );
