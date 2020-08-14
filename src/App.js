bf080b76910c1f97618ec8d56bd060cbca231d3b
KatyaKlema <katyaklema@gmail.com>
2020-08-14 23:48:50 +0300

    Changed package.json

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Sidebar from './components/sidebar'
 import Introduction from './components/introduction'
 import About from './components/about'
 import Timeline from './components/timeline'
-import { Helmet } from 'react-helmet'
 class App extends Component {
   render() {
     return (
       <div id="colorlib-page">
         <div id="container-wrap">
-            <Helmet>
-                <title>{"Kate Klem"}</title>
-            </Helmet>
          	<Sidebar></Sidebar>
 				<div id="colorlib-main">
 					<Introduction></Introduction>
